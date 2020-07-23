bb60bae35d0bc3ac8c047e079b45842aaaa8a30d
marita30 <marita.jose.ch@gmail.com>
2020-07-23 17:42:02 -0400

    Agregand javascript para  cerrar el navegadro lateral cuando hacemos clik en el fondo , y cuando hagamos click en el nuevo boton del nav que se despliegue el nav-mobile, para eso declaramos las dos var toglleButton y mobileNav con su querySelector y sus clases que se las definimos en el html, luego le decimos toggleButton.addEventListener('click', fuction(){ mobileNav.style.display = 'block' y asi mismo con el backdrop}, luego le agregamos al backdrop.addEventListener la function(){ mobileNav.style.display = none closeModal();}

--- Css/shared.js
+++ Css/shared.js
@@ -1,28 +1,40 @@
                                         /*Es la clase que le definimos en el html  */
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
-var modalNoButton = document.querySelector('.modal__action--negative')
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 
     /* Para los botones de los packages  */
     for (var i = 0; i < selectPlanButtons.length; i++) {
         selectPlanButtons[i].addEventListener('click', function() {
-            modal.style.display = 'block'
-            backdrop.style.display = 'block'
+            modal.style.display = 'block';
+            backdrop.style.display = 'block';
         });
     }
 
 
     /* Para cuando no le de click en el botton NO */
-    backdrop.addEventListener('click', closeModal);
+    backdrop.addEventListener('click', function() {
+        mobileNav.style.display = "none";
+        closeModal();
+    });
     modalNoButton.addEventListener('click', closeModal);
 
     function closeModal() {
-        backdrop.style.display = 'none'
-        modal.style.display = 'none'
+        backdrop.style.display = "none";
+        modal.style.display = "none";
         
     };
+
+    /* Botton del nav, para cerrar el navegador lateral cuando hacemos click en el fondo */
+    toggleButton.addEventListener('click', function() {
+        mobileNav.style.display = 'block';
+        backdrop.style.display = 'block';
+
+    });
 
 
 
