0ebd74698805b67b452dab198a3f91aa86503a66
marita30 <marita.jose.ch@gmail.com>
2020-07-23 14:47:23 -0400

    Ahora trabajamos para cambiar el estilo si el usuario quiere decir que no desea continuar o hace click para cerrar el modal le decimos que para backdrop.addEventListener('click', closeModal) y de igual forma para modalNoButto, luego function closeModal(){ backdrop.style.display= 'none' de igual forma para modal}, entonces le decimo que si el da click en no que cambie el display a none para ocultar el modal y el telon

--- Css/shared.js
+++ Css/shared.js
@@ -1,15 +1,29 @@
                                         /*Es la clase que le definimos en el html  */
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButtons = document.querySelectorAll('.plan button');
 
 
+    /* Para los botones de los packages  */
     for (var i = 0; i < selectPlanButtons.length; i++) {
         selectPlanButtons[i].addEventListener('click', function() {
             modal.style.display = 'block'
             backdrop.style.display = 'block'
         });
     }
+
+
+    /* Para cuando no le de click en el botton NO */
+    backdrop.addEventListener('click', closeModal);
+    modalNoButton.addEventListener('click', closeModal);
+
+    function closeModal() {
+        backdrop.style.display = 'none'
+        modal.style.display = 'none'
+        
+    };
+
 
 
 
