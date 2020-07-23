8c2d9bcf02143ddd3b4caa9f2ba59ac473b4e07c
marita30 <marita.jose.ch@gmail.com>
2020-07-23 14:26:24 -0400

    AGregando javascript para que cuado el usuario le de clik a cualquiera de los botones de los packages le aparezca el modal, para eso declaramos var backdrop = document.querySelector(.'backdrop'), var modal = document.querySelector('.modal') y var selectPlansButtons = document.querySelectorAll('.plan button) aqui le decimos all porque queremos que abarque los tres bottones, y luego recorremos esa matriz de for(var i = 0; i < selectPlanButtons.length; i++) { selectPlanButtons[i].addEventListener('click', fuction() { y aqui cambiamos el estilo por el addEventListener asi modal.style.display = 'block' y lo mismo con el el telon backdrop}})

--- Css/shared.js
+++ Css/shared.js
@@ -1,6 +1,19 @@
                                         /*Es la clase que le definimos en el html  */
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+
+
+    for (var i = 0; i < selectPlanButtons.length; i++) {
+        selectPlanButtons[i].addEventListener('click', function() {
+            modal.style.display = 'block'
+            backdrop.style.display = 'block'
+        });
+    }
+
+
+
 
 /* Reescribira las clases que tenemos en los archivos de css */
-backdrop.style.display = 'block'
-/* console.dir(backdrop); */
+/* backdrop.style.display = 'block' */
+/* console.dir(selectPlanButtons); */
