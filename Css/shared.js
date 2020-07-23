2a279407d81d5146484ee5fcd20d6204bcd285e0
marita30 <marita.jose.ch@gmail.com>
2020-07-23 14:02:15 -0400

    Agregando este archivo nuevo para utilizar javascripts para el modal y el telon de fondo backdrop

---
+++ Css/shared.js
@@ -0,0 +1,6 @@
+                                        /*Es la clase que le definimos en el html  */
+var backdrop = document.querySelector('.backdrop');
+
+/* Reescribira las clases que tenemos en los archivos de css */
+backdrop.style.display = 'block'
+/* console.dir(backdrop); */
